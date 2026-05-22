4c20d39e40e0abefecd727eb863b44322dfd943a
KhangLama <lamminhkhang123a@gmail.com>
2026-05-22 16:27:48 +0700

    revert: remove Google verification route handler

--- app/googlee95ca6b49c6191fb.html/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return new NextResponse("google-site-verification: googlee95ca6b49c6191fb.html", {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
-}
